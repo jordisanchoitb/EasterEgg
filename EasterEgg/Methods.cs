13e8ff91aa5e11827944129eeb3714962ae125a4
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2024-03-20 23:38:12 +0100

    EasterEgg terminado

--- EasterEgg/Methods.cs
+++ EasterEgg/Methods.cs
@@ -5,43 +5,101 @@ namespace EasterEgg
 {
     public static class Methods
     {
+        /// <summary>
+        /// Returns a character by its name
+        /// </summary>
+        /// <param name="name">Character Name</param>
+        /// <returns>Character</returns>
         public static Character GetCharacterByName(XMLHandler xmlHandler, string name)
         {
-            return xmlHandler.GetAllCharacters().FirstOrDefault(c => c.Name == name);
+            Character character = xmlHandler.GetAllCharacters().FirstOrDefault(c => c.Name == name);
+            if (character == null)
+            {
+                return xmlHandler.GetAllCharacters().FirstOrDefault(c => c.Name == "Pepe");
+            }
+            return character;
         }
 
-        public static void Fight(Character character1, Character character2)
+        /// <summary>
+        /// Battle between two characters
+        /// </summary>
+        /// <param name="character1">Your character</param>
+        /// <param name="character2">Aleatori character</param>
+        public static void Fight(Character character1, Character character2, XMLHandler xmlHandler)
         {
-            Console.WriteLine("Comença la batalla entre " + character1.Name + " i " + character2.Name);
+            const string MsgStartFight = "Comença la batalla entre {0} i {1}";
+            const string MsgTie = "Empat!";
+            const string MsgWin = "{0} ha guanyat la batalla!";
+            const string MsgLevelUp = "{0} ha pujat de nivell!";
+            const string MsgBattle = "Ataca {0} a {1}, amb un atac que li treu {2} de vida.\nVida restant a {3}: {4}";
+            const int DefaultAtk = 5, Zero = 0;
+
+            Console.WriteLine(MsgStartFight, character1.Name , character2.Name);
             int tempHp1 = character1.Hp;
             int tempHp2 = character2.Hp;
-            while (tempHp1 > 0 && tempHp2 > 0)
+            int calcAtkCharacter1 = (character1.Atk - character2.Def) == Zero ? DefaultAtk : (character1.Atk - character2.Def);
+            int calcAtkCharacter2 = (character2.Atk - character1.Def) == Zero ? DefaultAtk : (character2.Atk - character1.Def);
+
+            while (tempHp1 > Zero && tempHp2 > Zero)
             {
-                tempHp1 -= character2.Atk - character1.Def;
-                tempHp2 -= character1.Atk - character2.Def;
+                Console.WriteLine(MsgBattle, character2.Name, character1.Name, calcAtkCharacter2, character1.Name, tempHp1 - calcAtkCharacter2);
+                tempHp1 -= calcAtkCharacter2;
+                Console.WriteLine(MsgBattle, character1.Name, character2.Name, calcAtkCharacter1, character2.Name, tempHp2 - calcAtkCharacter1);
+                tempHp2 -= calcAtkCharacter1;
             }
-            if (tempHp1 <= 0 && tempHp2 <= 0)
+            if (tempHp1 <= Zero && tempHp2 <= Zero)
             {
-                Console.WriteLine("Empat!");
+                Console.WriteLine(MsgTie);
             }
-            else if (tempHp1 <= 0)
+            else if (tempHp1 <= Zero)
             {
-                Console.WriteLine(character2.Name + " ha guanyat la batalla!");
+                Console.WriteLine(MsgWin, character2.Name);
                 character2.Lvl++;
+                Console.WriteLine(MsgLevelUp, character2.Name);
+                xmlHandler.ModifyCharacter(character2);
             }
             else
             {
-                Console.WriteLine(character1.Name + " ha guanyat la batalla!");
+                Console.WriteLine(MsgWin, character1.Name);
                 character1.Lvl++;
+                Console.WriteLine(MsgLevelUp, character1.Name);
+                xmlHandler.ModifyCharacter(character1);
             }
-
         }
 
+        /// <summary>
+        /// Returns a random character
+        /// </summary>
+        /// <returns>Character</returns>
         public static Character GetRandomCharacter(XMLHandler xmlHandler)
         {
             List<Character> characters = xmlHandler.GetAllCharacters();
             var random = new Random();
             return characters[random.Next(characters.Count)];
+        }
+
+        /// <summary>
+        /// Returns a boolean if the message is yes or no
+        /// </summary>
+        /// <param name="message">user text</param>
+        /// <returns>bool</returns>
+        public static bool CheckYesNo(string message)
+        {
+            if (message.ToLower() == "yes" || message.ToLower() == "y" || message.ToLower() == "s" || message.ToLower() == "si")
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Returns a boolean if the value is greater or equal than the number especified
+        /// </summary>
+        /// <param name="number">Number comparable</param>
+        /// <param name="value">User value</param>
+        /// <returns></returns>
+        public static bool CheckGreaterOrEqualThanNumber(int number, int value)
+        {
+            return value >= number;
         }
     }
 }
--- EasterEgg/Program.cs
+++ EasterEgg/Program.cs
@@ -15,13 +15,49 @@ namespace EasterEgg
             const string MsgOpcio = "Que vols fer crear un personatge nou (0), jugar amb un ja creat (1) o modificar un ja creat (2). Per a sortir (3)";
             const string MsgOpcioNoValida = "Opcio no valida, torna a posar el valor";
             const string StartCreateCharacter = "Començem amb la creacio del personatge.";
-            const string CloseProgram = "Tancant el joc.Adeu!";
+            const string CloseProgram = "Tancant el joc. Adeu!";
+            const string DefaultName = "DefaultName";
+            const string MsgPlayWithCharacter = "Amb quin personatge vols jugar? Hi han aquestas opcions:";
+            const string MsgModifyCharacter = "Quin personatge vols modificar? Hi han aquestas opcions:";
+            const string NameDefaultCharacter = "Pepe";
+            const string MsgIntroduceNameGame = "Introdueix el seu nom, (en cas de equivocar-te amb el nom rebràs el personatge per defecte):";
+            const string MsgPlayOrNot = "Que vols fer, per sortir (1), si vols seguir jugat pulsa qualsevol numero que no sigui el (1)";
+            const string ClickToContinue = "Prem algun boto per continua";
+            const string GoToMainMenu = "Tornan al menu principal...";
+            const string NoCharacterCreatedByUser = "No hi ha cap personatge creat per el usuari si vols modificar valors d'algun personatge, te que ser creat per el usuari.\nEl personatge per defecte no es pot modificar.";
+            const string MsgIntroduceNameModify = "Introdueix el seu nom, (en cas de equivocar-te amb el nom rebràs sortiras al programa principal):";
+            const string CantModifyDefaultCharacter = "No pots modificar el personatge per defecte.";
+            const string MsgExistCharacter = "Ja existeix un personatge amb aquest nom.";
+            const string MsgTryAgain = "Intenta un altre cop.";
+            const string MsgChangeNameCorrectly = "Nom canviat correctament.";
+            const string MsgChangeLvlCorrectly = "Nivell canviat correctament.";
+            const string MsgChangeAtkCorrectly = "Atac canviat correctament.";
+            const string MsgChangeHpCorrectly = "Vida canviada correctament.";
+            const string MsgChangeDefCorrectly = "Defensa canviada correctament.";
+            const string MsgErrorInputLvl = "El nivell ha de ser major o igual a {0}.";
+            const string MsgErrorInputAtk = "L'atac ha de ser major o igual a {0}.";
+            const string MsgErrorInputHp = "La vida ha de ser major o igual a {0}.";
+            const string MsgErrorInputDef = "La defensa ha de ser major o igual a {0}.";
+            const string MsgModify = "Vols modificar el {0}? (si) o (no)";
+            const string MsgModifyGet = "Introdueix el nou {0}:";
+            const string MsgName = "nom";
+            const string MsgLvl = "nivell";
+            const string MsgAtk = "atac";
+            const string MsgHp = "vida";
+            const string MsgDef = "defensa";
+            const string MsgCharacterModified = "Personatge modificat correctament.";
+            const string MsgCreatingDefaultCharacter = "Creant un personatge per defecte.";
+            const string MsgCurrentValue = "Valor actual: {0}";
+
+
+            const int Zero = 0, MinHp = 10;
             int option;
             bool play = true;
             List<Character> characterList = xmlHandler.GetAllCharacters();
-            if (characterList.Count == 0)
+            if (characterList.Count == Zero)
             {
                 xmlHandler.AddCharacter(new Character());
+                Console.WriteLine(MsgCreatingDefaultCharacter);
             }
             while (play)
             {
@@ -49,31 +85,188 @@ namespace EasterEgg
 
                     case 1:
                         {
-                            // Jugar amb el personatje creat
-                            Console.WriteLine("Amb quin personatge vols jugar?");
-                            Console.WriteLine("Introdueix el seu nom:");
+                            // Jugar amb els personatges creats
+                            Console.WriteLine(MsgPlayWithCharacter);
+                            characterList = xmlHandler.GetAllCharacters();
+                            foreach (Character character in characterList)
+                            {
+                                Console.WriteLine(character.Name);
+                            }
+                            Console.WriteLine(MsgIntroduceNameGame);
                             string name = Console.ReadLine() ?? "";
                             Character mycharacter = Methods.GetCharacterByName(xmlHandler, name);
-                            Character randomcharacter = Methods.GetRandomCharacter(xmlHandler);
-                            Methods.Fight(mycharacter, randomcharacter);
+                            //Character randomcharacter = Methods.GetRandomCharacter(xmlHandler);
+                            Character character1 = Methods.GetCharacterByName(xmlHandler, "Pepe");
+                            Methods.Fight(mycharacter, character1, xmlHandler);
+                            int userOption;
+                            Console.WriteLine(MsgPlayOrNot);
+                            userOption = Convert.ToInt32(Console.ReadLine());
+                            if (userOption == 1)
+                            {
+                                play = false;
+                                Console.WriteLine(CloseProgram);
+                            }
                             break;
                         }
 
                     case 2:
                         {
-                            Character character = new Character();
-                            Console.WriteLine(MsgNom);
-                            character.Name = Console.ReadLine() ?? "";
-                            Console.WriteLine(MsgNivell);
-                            character.Lvl = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine(MsgAtac);
-                            character.Atk = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine(MsgVida);
-                            character.Hp = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine(MsgDefensa);
-                            character.Def = Convert.ToInt32(Console.ReadLine());
+                            // Modificar un personatge
+                            Console.WriteLine(MsgModifyCharacter);
+                            characterList = xmlHandler.GetAllCharacters();
+                            if (characterList.Count == Zero)
+                            {
+                                Console.WriteLine(NoCharacterCreatedByUser);
+                                Console.WriteLine(GoToMainMenu);
+                                Console.WriteLine(ClickToContinue);
+                                Console.ReadKey();
+                            }
+                            else
+                            {
+                                foreach (Character character in characterList)
+                                {
+                                    if (character.Name != NameDefaultCharacter)
+                                    {
+                                        Console.WriteLine(character.Name);
+                                    }
+                                }
+                                Console.WriteLine(MsgIntroduceNameModify);
+                                string name = Console.ReadLine() ?? "";
+                                Character mycharacter = Methods.GetCharacterByName(xmlHandler, name);
+                                if (mycharacter.Name == NameDefaultCharacter)
+                                {
+                                    Console.WriteLine(CantModifyDefaultCharacter);
+                                } else
+                                {
+                                    string optionModify;
+                                    Console.WriteLine(MsgModify, MsgName);
+                                    optionModify = Console.ReadLine() ?? "";
+                                    if (Methods.CheckYesNo(optionModify))
+                                    {
+                                        bool correctName = false;
+                                        do
+                                        {
+                                            Console.WriteLine(MsgCurrentValue, mycharacter.Name);
+                                            Console.WriteLine(MsgModifyGet, MsgName);
+                                            string newname = Console.ReadLine() ?? DefaultName;
+                                            Character character = Methods.GetCharacterByName(xmlHandler, newname);
+                                            if (character.Name != NameDefaultCharacter)
+                                            {
+                                                Console.WriteLine(MsgExistCharacter);
+                                                Console.WriteLine(MsgTryAgain);
+                                                correctName = true;
+                                            }
+                                            else
+                                            {
+                                                mycharacter.Name = newname;
+                                                correctName = false;
+                                            }
+                                        } while (correctName);
+                                        Console.WriteLine(MsgChangeNameCorrectly);
+                                    }
+                                    Console.WriteLine(MsgModify, MsgLvl);
+                                    optionModify = Console.ReadLine() ?? "";
+                                    if (Methods.CheckYesNo(optionModify))
+                                    {
+                                        bool correctLvl = false;
+                                        do
+                                        {
+                                            Console.WriteLine(MsgCurrentValue, mycharacter.Lvl);
+                                            Console.WriteLine(MsgModifyGet, MsgLvl);
+                                            int newlvl = Convert.ToInt32(Console.ReadLine());
+                                            if (Methods.CheckGreaterOrEqualThanNumber(Zero, newlvl))
+                                            {
+                                                mycharacter.Lvl = newlvl;
+                                                correctLvl = false;
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine(MsgErrorInputLvl, Zero);
+                                                Console.WriteLine(MsgTryAgain);
+                                                correctLvl = true;
+                                            }
+                                        } while (correctLvl);                                       
+                                        Console.WriteLine(MsgChangeLvlCorrectly);
+                                    }
+                                    Console.WriteLine(MsgModify, MsgAtk);
+                                    optionModify = Console.ReadLine() ?? "";
+                                    if (Methods.CheckYesNo(optionModify))
+                                    {
+                                        bool correctAtk = false;
+                                        do
+                                        {
+                                            Console.WriteLine(MsgCurrentValue, mycharacter.Atk);
+                                            Console.WriteLine(MsgModifyGet, MsgAtk);
+                                            int newatk = Convert.ToInt32(Console.ReadLine());
+                                            if (Methods.CheckGreaterOrEqualThanNumber(Zero, newatk))
+                                            {
+                                                mycharacter.Atk = newatk;
+                                                correctAtk = false;
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine(MsgErrorInputAtk, Zero);
+                                                Console.WriteLine(MsgTryAgain);
+                                                correctAtk = true;
+                                            }
+                                        } while (correctAtk);
+                                        Console.WriteLine(MsgChangeAtkCorrectly);
+                                    }
+                                    Console.WriteLine(MsgModify, MsgHp);
+                                    optionModify = Console.ReadLine() ?? "";
+                                    if (Methods.CheckYesNo(optionModify))
+                                    {
+                                        bool correctHp = false;
+                                        do
+                                        {
+                                            Console.WriteLine(MsgCurrentValue, mycharacter.Hp);
+                                            Console.WriteLine(MsgModifyGet, MsgHp);
+                                            int newhp = Convert.ToInt32(Console.ReadLine());
+                                            if (Methods.CheckGreaterOrEqualThanNumber(MinHp, newhp))
+                                            {
+                                                mycharacter.Hp = newhp;
+                                                correctHp = false;
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine(MsgErrorInputHp, MinHp);
+                                                Console.WriteLine(MsgTryAgain);
+                                                correctHp = true;
+                                            }
+                                        } while (correctHp);
+                                        Console.WriteLine(MsgChangeHpCorrectly);
+                                    }
+                                    Console.WriteLine(MsgModify, MsgDef);
+                                    optionModify = Console.ReadLine() ?? "";
+                                    if (Methods.CheckYesNo(optionModify))
+                                    {
+                                        bool correctDef = false;
+                                        do
+                                        {
+                                            Console.WriteLine(MsgCurrentValue, mycharacter.Def);
+                                            Console.WriteLine(MsgModifyGet, MsgDef);
+                                            int newdef = Convert.ToInt32(Console.ReadLine());
+                                            if (Methods.CheckGreaterOrEqualThanNumber(Zero, newdef))
+                                            {
+                                                mycharacter.Def = newdef;
+                                                correctDef = false;
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine(MsgErrorInputDef, Zero);
+                                                Console.WriteLine(MsgTryAgain);
+                                                correctDef = true;
+                                            }
+                                        } while (correctDef);
+                                        Console.WriteLine(MsgChangeDefCorrectly);
+                                    }
+                                    xmlHandler.ModifyCharacter(mycharacter);
+                                    Console.WriteLine(MsgCharacterModified);
+                                }
+                            }
                             break;
                         }
+
                     case 3:
                         Console.WriteLine(CloseProgram);
                         play = false;
